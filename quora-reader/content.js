378cd89bb5037523c77db6f3be0471a630664e2e
Khen Daniel <thekhenzie@gmail.com>
2017-10-03 10:09:49 +0800

    Added if else

--- quora-reader/content.js
+++ quora-reader/content.js
@@ -3,7 +3,7 @@ chrome.extension.onMessage.addListener(function (message, sender, sendResponse) 
 		case "remove-header":
 			var divs = document.querySelectorAll('div.LoggedInSiteHeader, div.layout_3col_left, div.layout_3col_right,div.layout_2col_side');
 			if (divs.length === 0) {
-				console.log("There are no any divs in the page.");
+				
 			} else {
 				for (var i = 0; i < divs.length; i++) {
 					divs[i].style.display = "none";
@@ -12,7 +12,7 @@ chrome.extension.onMessage.addListener(function (message, sender, sendResponse) 
 
 			var mainDiv = document.querySelectorAll('div.layout_2col_main');
 			if (mainDiv.length === 0) {
-				console.log("There are no any divs in the page.");
+				
 			} else {
 				for (var i = 0; i < divs.length; i++) {
 				}
@@ -20,7 +20,7 @@ chrome.extension.onMessage.addListener(function (message, sender, sendResponse) 
 
 			var centerDiv = document.querySelectorAll('div.layout_3col_center');
 			if (centerDiv.length === 0) {
-				console.log("There are no any divs in the page.");
+				
 			} else {
 				for (var i = 0; i < divs.length; i++) {
 					divs[i].style.marginLeft = "0px";
@@ -29,7 +29,7 @@ chrome.extension.onMessage.addListener(function (message, sender, sendResponse) 
 
 			var temp = document.querySelectorAll("img.portrait,img.landscape");
 			if (temp.length === 0) {
-				console.log("There are no any divs in the page.");
+				
 			} else {
 				for (var i = 0; i < temp.length; i++) {
 					temp[i].style.width = "200px";
@@ -37,8 +37,8 @@ chrome.extension.onMessage.addListener(function (message, sender, sendResponse) 
 			}
 
 			var spans = document.querySelectorAll('span.rendered_qtext');
-			if (spans.length === 0) {
-				console.log("There are no any spans in the page.");
+			if (temp.length === 0) {
+				
 			} else {
 				for (var i = 0; i < spans.length; i++) {
 					spans[i].style.fontSize = "medium";
@@ -46,12 +46,20 @@ chrome.extension.onMessage.addListener(function (message, sender, sendResponse) 
 				}
 			}
 			var footerDiv = document.querySelectorAll('div.action_bar_inner, div.u-flex');
-			for (var i = 0; i < footerDiv.length; i++) {
-				footerDiv[i].style.display = "none";
+			if (temp.length === 0) {
+				
+			} else {
+				for (var i = 0; i < footerDiv.length; i++) {
+					footerDiv[i].style.display = "none";
+				}
 			}
 			var author = document.querySelectorAll('div.answer_user, div.answer_user_inline');
-			for (var i = 0; i < author.length; i++) {
-				author[i].style.display = "none";
+			if (temp.length === 0) {
+				
+			} else {
+				for (var i = 0; i < author.length; i++) {
+					author[i].style.display = "none";
+				}
 			}
 			break;
 	}
